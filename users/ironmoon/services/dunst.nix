52aa6fc4e57c603c64da3b52c0bbd02d835f7e9c
IRONM00N <me@ironmoon.dev>
2025-03-07 22:59:19 -0500

    remove dunst offset

--- users/ironmoon/services/dunst.nix
+++ users/ironmoon/services/dunst.nix
@@ -2,7 +2,14 @@
 {
   enable = true;
 
-  # settings = {
-    
-  # };
+  settings = {
+    global = {
+      enable_posix_regex = true;
+
+      follow = "mouse";
+
+      origin = "top-right";
+      offset = "(0, 0)"; # doesn't include waybar (won't overlap)
+    };
+  };
 }

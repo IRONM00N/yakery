e4fdf59ab3d3264792ef8d149d7f99b5f23f2aee
IRONM00N <me@ironmoon.dev>
2025-04-02 16:48:28 -0400

    don't suspend on desktop

--- users/ironmoon/services/hypridle.nix
+++ users/ironmoon/services/hypridle.nix
@@ -1,4 +1,4 @@
-{ info, ... }:
+{ info, pkgs, ... }:
 {
   enable = true;
   settings = {
@@ -8,30 +8,31 @@
       after_sleep_cmd = "hyprctl dispatch dpms on";
     };
 
-    listener = [
-      {
-        timeout = if info.laptop then 0.5 * 60 else 5 * 60;
-        on-timeout = "brightnessctl -s set 10";
-        on-resume = "brightnessctl -r";
-      }
-      {
-        timeout = if info.laptop then 2.5 * 60 else 10 * 60;
-        on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-        on-resume = "brightnessctl -rd rgb:kbd_backlight";
-      }
-      {
-        timeout = if info.laptop then 5 * 60 else 30 * 60;
-        on-timeout = "loginctl lock-session";
-      }
-      {
-        timeout = if info.laptop then 5.5 * 60 else 30.5 * 60;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
-      {
-        timeout = if info.laptop then 30 * 60 else 2 * 60 * 60;
+    listener =
+      [
+        {
+          timeout = if info.laptop then 0.5 * 60 else 5 * 60;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = if info.laptop then 2.5 * 60 else 10 * 60;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
+        {
+          timeout = if info.laptop then 5 * 60 else 30 * 60;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = builtins.floor (if info.laptop then 5.5 * 60 else 30.5 * 60);
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ]
+      ++ pkgs.lib.optional info.laptop {
+        timeout = 30 * 60;
         on-timeout = "systemctl suspend";
-      }
-    ];
+      };
   };
 }

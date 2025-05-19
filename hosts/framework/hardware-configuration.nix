3fca2d10c4fb93d2da2d9e51e93c0517ad8fe420
IRONM00N <me@ironmoon.dev>
2025-05-19 13:43:21 -0400

    framework fixes: remove invalid cpufreq_stats, add swap

--- hosts/framework/hardware-configuration.nix
+++ hosts/framework/hardware-configuration.nix
@@ -39,7 +39,9 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/dev/nvme0n1p4"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

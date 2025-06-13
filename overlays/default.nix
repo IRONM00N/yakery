a683672b3bf32220e50d8c8aadd1a535e7182f87
IRONM00N <me@ironmoon.dev>
2025-06-13 10:54:30 -0400

    remove old overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,2 @@
 [
-  (import ./paper.nix)
 ]
--- overlays/paper.nix
+++
@@ -1,8 +0,0 @@
-final: prev: {
-  papermc = prev.papermc.overrideAttrs (o: {
-    src = prev.fetchurl {
-      url = "https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/79/downloads/paper-1.21.4-79.jar";
-      hash = "sha256-9FSZAhgn+nhwIuj5KXNnRHlVytgZA+k+RDD1twc9EdA=";
-    };
-  });
-}
